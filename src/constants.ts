c8b3050e0d5ff6c35d3ba8e3cb53cfe475cbacce
somin0016 <tsoominn@gmail.com>
2024-04-19 00:43:35 +0800

    add ssr

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@
 export const SITE_TITLE = 'Soe Min Thant';
 export const SITE_DESCRIPTION = `Soemin's portfolio site!`;
 export const API_BASE_URL = "https://blog.smthant.com/graphql";
+export const WORDPRESS_BLOG_URL = "https://blog.smthant.com/";
