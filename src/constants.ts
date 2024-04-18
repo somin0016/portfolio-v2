eec13711efd5f13c3902e49f01c8079e9e7a049f
somin0016 <tsoominn@gmail.com>
2024-04-18 22:45:10 +0800

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+// All global variables used within the site
+export const SITE_TITLE = 'Soe Min Thant';
+export const SITE_DESCRIPTION = `Soemin's portfolio site!`;
+export const API_BASE_URL = "https://blog.smthant.com/graphql";
---
+++ tailwind.config.mjs
@@ -0,0 +1,54 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+			fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        smPrimary: {
+          DEFAULT: "#183367",
+          strong: "#0e1f3e",
+          light: "#224790",
+        },
+        smSecondary: {
+          DEFAULT: "#97b2e7",
+        },
+        smWhite: {
+          DEFAULT: "#FBFBFB",
+        },
+        smBorder: {
+          DEFAULT: "#dddee2",
+        },
+        smText: {
+          DEFAULT: "#232529",
+          light: "#5e646e",
+        },
+        smBackground: {
+          // DEFAULT: '#efeff1',
+          DEFAULT: "#F5F5F5",
+        },
+        smBlue: {
+          DEFAULT: "#0D99FF",
+          strong: "#0A7ACC",
+        },
+        smYellow: {
+          DEFAULT: "#FFCD29",
+          strong: "#CCA421",
+        },
+        smRed: {
+          DEFAULT: "#F24822",
+          strong: "#C23A1B",
+        },
+        smGreen: {
+          DEFAULT: "#14AE5C",
+          strong: "#108B4A",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
