b790051c3ef109f519415b60aa3d9f569725d1e5
somin0016 <tsoominn@gmail.com>
2024-04-23 12:51:39 +0800

    increase limit rate

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,10 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
 });
