475f96346c3b54a9bc9e78e5627c6c7997a41b17
somin0016 <tsoominn@gmail.com>
2024-05-20 17:33:40 +0800

    fix wp url in constant

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ export const SITE_DESCRIPTION = `Soemin's portfolio site!`;
 
 // ENV
 export const API_BASE_URL = import.meta.env.PUBLIC_API_BASE_URL;
-export const WORDPRESS_BLOG_URL = import.meta.env.WORDPRESS_BLOG_URL;
+export const WORDPRESS_BLOG_URL = import.meta.env.PUBLIC_WORDPRESS_BLOG_URL;
 export const LASTFM_API_KEY = import.meta.env.PUBLIC_LASTFM_API_KEY
