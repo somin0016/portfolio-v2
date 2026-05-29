f5f187b46f80cf13da4ff0641b221457ffbd1356
Soe Min <tsoominn@gmail.com>
2026-05-29 20:46:24 +0800

    update projects page design

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,8 +7,18 @@ const project = defineCollection({
 		title: z.string(),
 		description: z.string(),
     year: z.number(),
+		index: z.number().optional(),
 		background: z.string(),
 		heroImage: z.string().optional(),
+		skills: z.array(z.string()).optional(),
+		links: z
+			.array(
+				z.object({
+					name: z.string(),
+					url: z.string(),
+				})
+			)
+			.optional(),
 	}),
 });
 
